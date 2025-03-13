aa225634281564b1dd4c6b504b8b427fa9e3bd3d
harrylbi <122418589+harrylbi@users.noreply.github.com>
2025-03-13 20:12:18 +0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -3,39 +3,18 @@ import java.util.Random;
 
 public class Game {
     private int targetNumber;
-    private boolean isRunning;
+    private boolean Run;
     private Player player;
     private Scanner scanner;
 
     public Game() {
         Random random = new Random();
         targetNumber = random.nextInt(100) + 1; 
-        isRunning = true;
+        Run = true;
         player = new Player();
         scanner = new Scanner(System.in);
     }
 
-    public void start() {
-        System.out.println("=== Game Tebak Angka ===");
-        System.out.println("Tebak angka antara 1 - 100!");
-
-        while (isRunning) {
-            System.out.print("Masukkan tebakan: ");
-            int guess = scanner.nextInt();
-            player.setGuess(guess);
-
-            if (player.getGuess() == targetNumber) {
-                System.out.println("Selamat! Anda menebak dengan benar.");
-                isRunning = false;
-            } else if (player.getGuess() < targetNumber) {
-                System.out.println("Tebakan terlalu rendah, coba lagi!");
-            } else {
-                System.out.println("Tebakan terlalu tinggi, coba lagi!");
-            }
-        }
-
-        System.out.println("Game selesa");
-    }
 
     public void start() {
     System.out.println("=== Game Tebak Angka ===");
